12b6250e6af5a463ca53de41c9a43529062a1377
William Angulo <wmavalle@gmail.com>
2021-04-03 17:56:37 -0500

    "Usable matrialize"

--- config/passport.js
+++ config/passport.js
@@ -6,15 +6,32 @@ const User = require('../models/User')
 module.exports = function(passport){
     passport.use( 
         new GoogleStrategy(
-       {
+      {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: '/auth/google/callback',
-       },
+      },
       async (accessToken, refreshToken, profile,done)=> {
-         console.log(profile)
-         
+        const newUser = {
+          googgleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value,
         }
+          try{
+              let user = await User.findOne({googleId: profile.id})
+                if(user){
+                  done(null,user)
+                } else{
+                  user = await User.create(newUser)
+                  done(null,user)   
+                  }
+        }catch(err ){
+            console.error(err)
+        }
+
+      }
     )
   )
 
--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    iamge: {
+    image: {
         type: String,
         
     },
